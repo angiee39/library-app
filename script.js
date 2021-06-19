448eea2981898496873533bfbb149b0ae4599267
angiee39 <anjalarox@gmail.com>
2021-06-19 15:29:32 +0530

    Completed task by completing part 6. Needs to add better UI.

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-let myLibrary = [];
+let myLibrary = [{ title: "Harry Potter 1", author: "J.K. Rowling", pages: "567", read: "Read" }];
 
 let title;
 let author;
 let pages;
 let read;
 
-// let title, author, pages, read;
-
+// Book constructor
 function Book(a, b, c, d) {
-    // the Constructor
     this.title = a
     this.author = b
     this.pages = c
     this.read = d
 }
 
+// Add book to library
 function addBookToLibrary() {
     const newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook)
 }
+
+// Remove book from librar
+function removeBook(a) {
+    myLibrary.splice(a, 1)
+    showTable()
+}
+
+// Toggle read/unread
+function toggleRead (a) {
+    if (myLibrary[a]['read'] === 'Read') {
+        myLibrary[a]['read'] = 'Unread'
+    } else {
+        myLibrary[a]['read'] = 'Read'
+    }
+    showTable()
+}
+
+// Print table
+function showTable() {
+    tbody.innerHTML = ''
+    for (let i = 0; i < myLibrary.length; i++) {
+        tbody.innerHTML += '<tr><td>' + myLibrary[i]['title'] + '</td><td>' + myLibrary[i]['author'] 
+        + '</td><td>' + myLibrary[i]['pages'] + `</td><td id="read${i}">` + myLibrary[i]['read'] + `</td><td id="book${i}">Delete</td></tr>`;
+    }
+    readyRemove()
+    readyRead()
+}
+
 
 // All querySelectors
 const addDiv = document.querySelector('#addDiv')
@@ -27,9 +54,7 @@ const formDiv = document.querySelector('#formDiv')
 const addBtn = document.querySelector("#addBtn")
 const submit = document.querySelector("#submit")
 const back = document.querySelector("#back")
-const tr = document.querySelector('#tr')
-
-
+const tbody = document.querySelector('#tbody')
 
 
 // All eventListners
@@ -54,23 +79,33 @@ submit.addEventListener('click', () => {
     addDiv.classList.remove('hide')
     tableDiv.classList.remove('hide')
     formDiv.classList.add('hide')
-    console.log(title, author, pages, read)
-    console.log(myLibrary)
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i]['title'])
-        tr.innerHTML += '<tr>'
-        tr.innerHTML += `   <td>${myLibrary[i]['title']}</td>`
-        tr.innerHTML += `   <td>${myLibrary[i]['author']}</td>`
-        tr.innerHTML += `   <td>${myLibrary[i]['pages']}</td>`
-        tr.innerHTML += `   <td>${myLibrary[i]['read']}</td>`
-        tr.innerHTML += '</tr>'
-    
-    }
-    
-    
+    showTable()
 });
 
+showTable()
 
+function readyRemove() {
+    // Listen for delete buttons
+    let elements = [];
 
+    for (let i = 0; i < myLibrary.length; i++) {
+        elements.push(document.querySelector(`#book${i}`))
+        elements[i].addEventListener('click', () => {
+            removeBook(i)
+        });
+    }
+}
+
+function readyRead() {
+    // Listen for read/unread buttons
+    let elements = [];
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        elements.push(document.querySelector(`#read${i}`))
+        elements[i].addEventListener('click', () => {
+            toggleRead(i)
+        });
+    }
+}
 
 
